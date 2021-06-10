e72e1f3ef47d762d7d850211e4e4d56f38f126ac
hojiakbar-97 <82793852+hojiakbar-97@users.noreply.github.com>
2021-06-10 18:05:47 +0500

    commit

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import './todo-list-item.css'
+
+export default  class TodoListItem extends Component{
+
+    render() {
+        const { label, onDeleted,
+                onToggleImportant,
+                onToggleDone,
+                important, done } = this.props;
+        let classNames = 'todo-list-item';
+        if (done){
+            classNames += ' done';
+        }
+        if (important) {
+            classNames += ' important';
+        }
+
+
+        return (
+            <span className={classNames}>
+            <span className="todo-list-item-label"
+                  onClick={ onToggleDone }>
+                  { label }
+            </span>
+
+            <button type="button" className="btn float-right btn-outline-success btn-sm"
+                    onClick={onToggleImportant}>
+                <i className="fa fa-exclamation" />
+            </button>
+
+            <button type="button" className="btn float-right btn-outline-danger btn-sm" onClick={onDeleted}>
+                <i className="fa fa-trash-o" />
+            </button>
+        </span>
+        );
+    }
+}
